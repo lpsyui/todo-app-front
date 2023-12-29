fe64c33bcedf3ef44decea3edba1772cb6c8633f
lpsyui <lpsyui@yahoo.co.jp>
2023-12-29 23:21:11 +0900

    update 取得API

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,17 @@
+<!-- src/App.svelte -->
 <script>
-	export let name;
+	import FetchData from "./FetchData.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<h2>TODO APP</h2>
-
-	<!-- App.svelte -->
-	<!-- App.svelte -->
-	<script>
-		import TodoList from "./TodoList.svelte";
-	</script>
-
-	<TodoList />
+	<h1>Hello Svelte</h1>
+	<FetchData />
+	<!-- 他のコンポーネントや要素も追加できます -->
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	h2 {
-		color: pink;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 50;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		padding: 2em;
 	}
 </style>
---
+++ src/FetchData.svelte
@@ -0,0 +1,26 @@
+<!-- src/components/FetchData.svelte -->
+<script>
+    let message = "";
+
+    async function fetchData() {
+        try {
+            const response = await fetch("http://localhost:8090/api/hello");
+            const data = await response.json();
+            message = data.message;
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+</script>
+
+<main>
+    <h2>{message}</h2>
+    <button on:click={fetchData}>Fetch Data</button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 2em;
+    }
+</style>
